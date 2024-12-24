9eb897f697b39c3d93fe60270240af577b4da1b8
RVlad <reshetov664@gmail.com>
2024-12-24 09:23:36 +0700

    patch to get_mask_card()

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,16 @@
 def get_mask_card_number(card_number: str) -> str:
     """Принимает номер карты и возвращает ее маску"""
 
-    return f"{card_number[:len(card_number) - 16]} {card_number[-16:-12]} {card_number[-11:-13]}** **** {card_number[-4:]}"
+    return f"{card_number[:len(card_number) - 16]} {card_number[-16:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(card_mask: str) -> str:
+    """принимает номер счета и возвращает его маску"""
+
+    return f"{card_mask[:len(card_mask) - 20]} **{card_mask[-4:]}"
+
 
 
 if __name__ == "__main__":
     print(get_mask_card_number(input()))
+    print(get_mask_account(input()))
