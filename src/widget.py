75de199f31114ef943c577abbdac8c27a047f789
RVlad <reshetov664@gmail.com>
2025-01-20 03:23:49 +0700

    омашка 10.2 елиз - 0. онедельник, 20 января 2025 г., 03:23:2

--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,12 @@
-
-def get_mask_card_number(card_number: str) -> str:
-    """Принимает номер карты и возвращает ее маску"""
-
-    return f"{card_number[:len(card_number) - 16]} {card_number[-16:-12]} {card_number[-12:-10]}** **** {card_number[-4:
-    ]}"
+from masks import get_mask_card_number, get_mask_account
 
 
-def get_mask_account(card_mask: str) -> str:
-    """принимает номер счета и возвращает его маску"""
-
-    return f"{card_mask[:len(card_mask) - 20]} **{card_mask[-4:]}"
+def get_mask_account_card(card_mask: str) -> str:
+    """принимает номер либо номер карты либо счета и возвращает их особенную маску маску"""
+    if len(card_mask) == 16:
+        return get_mask_card_number(card_mask)
+    elif len(card_mask) == 20:
+        return get_mask_account(card_mask)
 
 
 def get_date(date: str) -> str:
@@ -20,6 +17,6 @@ def get_date(date: str) -> str:
 
 
 if __name__ == "__main__":
-    print(get_mask_card_number(input()))
-    print(get_mask_account(input()))
-    print(get_date(input()))
+    # print(get_mask_card_number(input()))
+    print(get_mask_account_card(input()))
+    # print(get_date(input()))
