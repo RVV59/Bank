7836cf823dee6cbdccf9071153e9c993c995a2cd
RVlad <reshetov664@gmail.com>
2025-01-19 23:21:11 +0700

    mask.py is done оскресенье, 19 января 2025 г., 23:21:01

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 from src.masks import get_mask_card_number, get_mask_account, get_date
 
 
@@ -20,6 +21,7 @@ def test_get_mask_card_number(card_number, expected_exception):
         with pytest.raises(expected_exception):
             get_mask_card_number(card_number)
 
+
 @pytest.mark.parametrize("card_mask,expected_output",
 [
         ("12341234123412345555", None),
@@ -27,7 +29,7 @@ def test_get_mask_card_number(card_number, expected_exception):
         ("", ValueError),
         ("123412341234123X1111", ValueError),
         ("ABCDEF12345678901111", ValueError),
-    ],
+],
 )
 def test_get_mask_account(card_mask, expected_output):
     if expected_output is None:
@@ -37,22 +39,11 @@ def test_get_mask_account(card_mask, expected_output):
             get_mask_account(card_mask)
 
 
-#     ("11111234567890123456", "**3456"),
-#     ("11110987654321098765", "**8765")
-# ])
-# def test_get_mask_account(input_account_number, expected_output):
-#     actual_output = get_mask_account(input_account_number)
-#     assert actual_output == expected_output
-#
+def test_get_date():
+    date = '2024-03-11T02:26:18.671407'
+    result = get_date(date)
+    assert result == '11.03.2024', "Результат должен быть '11.03.2024'"
 
-
-# @pytest.mark.parametrize("input_date,expected_output", [
-#     ("2023-10-15", "15.10.2023"),
-#     ("2000-01-01", "01.01.2000")
-# ])
-# def test_get_date(input_date, expected_output):
-#     actual_output = get_date(input_date)
-#     assert actual_output == expected_output
 @pytest.fixture
 def valid_dates():
     return [
@@ -74,6 +65,7 @@ def test_get_date_with_valid_dates(valid_dates):
         result = get_date(date)
         assert isinstance(result, str), "Результат должен быть строкой."
         assert len(result.split('.')) == 3, "Результат должен содержать три части."
+
 
 def test_get_date_with_invalid_dates(invalid_dates):
     for date in invalid_dates:
