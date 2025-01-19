3a1f3805bc653851040a44d042fd95206d858622
RVlad <reshetov664@gmail.com>
2025-01-19 13:29:47 +0700

    оскресенье, 19 января 2025 г., 13:29:31

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -46,10 +46,38 @@ def test_get_mask_account(card_mask, expected_output):
 #
 
 
-@pytest.mark.parametrize("input_date,expected_output", [
-    ("2023-10-15", "15.10.2023"),
-    ("2000-01-01", "01.01.2000")
-])
-def test_get_date(input_date, expected_output):
-    actual_output = get_date(input_date)
-    assert actual_output == expected_output
+# @pytest.mark.parametrize("input_date,expected_output", [
+#     ("2023-10-15", "15.10.2023"),
+#     ("2000-01-01", "01.01.2000")
+# ])
+# def test_get_date(input_date, expected_output):
+#     actual_output = get_date(input_date)
+#     assert actual_output == expected_output
+@pytest.fixture
+def valid_dates():
+    return [
+        '2024-03-11T02:26:18.671407',
+        '2000-01-01T00:00:00.000000'
+    ]
+
+@pytest.fixture
+def invalid_dates():
+    return [
+        '',
+        'invalid-date-format',
+        '2024-03-11T02:26:18.67140',
+        '2024-03-11T02:26:18.6714077'
+    ]
+
+def test_get_date_with_valid_dates(valid_dates):
+    for date in valid_dates:
+        result = get_date(date)
+        assert isinstance(result, str), "Результат должен быть строкой."
+        assert len(result.split('.')) == 3, "Результат должен содержать три части."
+
+def test_get_date_with_invalid_dates(invalid_dates):
+    for date in invalid_dates:
+        with pytest.raises(ValueError):
+            get_date(date)
+
+
