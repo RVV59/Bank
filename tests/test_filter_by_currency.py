7aded38420b8f3667920c2e060383f75743437b8
RVlad <reshetov664@gmail.com>
2025-01-25 11:59:37 +0700

    functions are ready Суббота, 25 января 2025 г., 11:58:20

--- tests/test_filter_by_currency.py
+++ tests/test_filter_by_currency.py
@@ -1,22 +1,25 @@
 import pytest
 
+from src.generators import filter_by_currency
+
+
 @pytest.fixture
 def usd_transactions():
     return list(filter_by_currency(transactions, "USD"))
 
+
 @pytest.fixture
 def eur_transactions():
-    return list(filter_by_currency(transactions, "EUR"))# Параметризация для проверки различных состояний транзакций
+    return list(filter_by_currency(transactions, "EUR"))
+
 
 @pytest.mark.parametrize("states", [("EXECUTED", "FAILED")])
 def test_filter_by_currency(states):
-    # Проверка для успешного состояния транзакций
     if states == "EXECUTED":
         assert len(usd_transactions) == 2
         assert "USD" in usd_transactions[0]["operationAmount"]["currency"].values()
         assert "USD" in usd_transactions[1]["operationAmount"]["currency"].values()
 
-    # Проверка для неудачного состояния транзакций
     elif states == "FAILED":
         assert len(eur_transactions) == 1
         assert "EUR" in eur_transactions[0]["operationAmount"]["currency"].values()
--- tests/test_transaction_discriptions.py
+++ tests/test_transaction_discriptions.py
@@ -1,14 +1,15 @@
 import pytest
 
+from src.generators import transaction_descriptions
+
 
 @pytest.fixture
 def descriptions():
-    return list(transaction_descriptions(transactions))# Параметризация для проверки различных типов описаний транзакций
+    return list(transaction_descriptions(transactions), "")
 
-@pytest.fixture
-def descriptions():
-    return list(transaction_descriptions(transactions))
 
-@pytest.mark.parametrize("descriptions", [("Перевод организации", "Перевод со счета на счет", "Перевод с карты на карту")])
+@pytest.mark.parametrize("descriptions", [("Перевод организации", "Перевод со счета на счет",
+                                           "Перевод с карты на карту"
+                                           , "")])
 def test_transaction_descriptions(descriptions):
     assert descriptions == descriptions
