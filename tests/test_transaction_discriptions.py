2bb7d42a2680074d4cac4892b54f1682011f8446
RVlad <reshetov664@gmail.com>
2025-01-26 18:29:58 +0700

    thank to Ivan-2 Воскресенье, 26 января 2025 г.

--- tests/test_transaction_discriptions.py
+++ tests/test_transaction_discriptions.py
@@ -3,13 +3,24 @@ import pytest
 from src.generators import transaction_descriptions
 
 
-@pytest.fixture
-def descriptions():
-    return list(transaction_descriptions(transactions), "")
+# @pytest.fixture
+# def descriptions():
+#     return list(transaction_descriptions(transactions), "")
+#
+#
+# @pytest.mark.parametrize("descriptions", [("Перевод организации", "Перевод со счета на счет",
+#                                            "Перевод с карты на карту"
+#                                            , "")])
+# def test_transaction_descriptions(descriptions):
+#     assert descriptions == descriptions
 
 
-@pytest.mark.parametrize("descriptions", [("Перевод организации", "Перевод со счета на счет",
-                                           "Перевод с карты на карту"
-                                           , "")])
-def test_transaction_descriptions(descriptions):
-    assert descriptions == descriptions
+@pytest.mark.parametrize(
+    "data, expected", [([{"description": "Перевод организации",},
+                         {"wrong_key": "Перевод со счета на счет",},
+                         {"description": "Перевод с карты на карту",},],
+                         ["Перевод организации", "", "Перевод с карты на карту"]),
+                       ([], [])]
+)
+def test_transaction_descriptions(data, expected):
+    asseZrt list(transaction_descriptions(data)) == expected
